34ec9daaa56fb3d91eb1b3b017bd79562699d1db
EvertonAvelino <everton-vitor@hotmail.com>
2022-06-03 00:30:51 -0300

    Primeira parte

---
+++ expogo/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Routes from './src/routes';
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <StatusBar backgroundColor={'#232330'} />
+      <Routes />
+    </NavigationContainer>
+  )
+
+};
